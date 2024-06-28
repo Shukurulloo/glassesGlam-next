c97ac70947d20ec886b775c72e916e74e6118bd7
mac <mac@macui-MacBookPro.local>
2024-06-28 18:07:11 +0900

    fix: modify Chatting related design

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -141,7 +141,7 @@ const withLayoutBasic = (Component: any) => {
 							<Component {...props} />
 						</Stack>
 							{/* chatting */}
-						{user?._id && <Chat />}
+						<Chat />
 
 						<Stack id={'footer'}>
 							<Footer />
